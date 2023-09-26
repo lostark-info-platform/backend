e7e201d2e3e74dcaceefac4fbb2f5fdd397fda0c
Duhan Mo <moduhan95@gmail.com>
2023-09-26 17:38:44 +0900

    test: ResignServiceTest를 추가한다

--- src/test/kotlin/org/info/lostark/application/ResignServiceTest.kt
+++ src/test/kotlin/org/info/lostark/application/ResignServiceTest.kt
@@ -1,5 +1,6 @@
 package org.info.lostark.application
 
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.mockk.Runs
 import io.mockk.clearAllMocks
@@ -8,6 +9,7 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import org.info.lostark.domain.user.Password
+import org.info.lostark.domain.user.UnidentifiedUserException
 import org.info.lostark.domain.user.UserRepository
 import org.info.lostark.domain.user.getOrThrow
 import org.info.lostark.fixture.createUser
@@ -23,11 +25,22 @@ class ResignServiceTest : BehaviorSpec({
         every { userRepository.getOrThrow(any()) } returns createUser()
         every { refreshTokenService.revokeAll(any()) } just Runs
 
-        When("회원탈퇴 하면") {
+        When("올바른 비밀번호로 회원탈퇴 하면") {
             resignService.resign(1L, ResignRequest(Password("password")))
 
             Then("리프레쉬토큰을 삭제한다") {
                 verify(exactly = 1) { refreshTokenService.revokeAll(1L) }
+            }
+        }
+
+        When("올바르지 않은 비밀번호로 회원탈퇴 하면") {
+            Then("에러 발생한다") {
+                shouldThrow<UnidentifiedUserException> {
+                    resignService.resign(
+                        1L,
+                        ResignRequest(Password("anotherPassword"))
+                    )
+                }
             }
         }
     }
